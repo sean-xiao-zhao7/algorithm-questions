ba664cbb473f740e769d84df4a04a7a626463c36
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-08-17 11:51:36 -0400

    Implement reverse string using swapping

--- reverse_string.py
+++ reverse_string.py
@@ -1,8 +1,14 @@
 print('This is the reverse string program.')
 user_input = input("Enter the input string:")
 
-result = ''
-for char in user_input:
-    result = char + result
+result = list(user_input)
+front_pointer = 0
+back_pointer = len(user_input) - 1
+while front_pointer < back_pointer:
+    temp = result[front_pointer]
+    result[front_pointer] = result[back_pointer]
+    result[back_pointer] = temp
+    front_pointer += 1
+    back_pointer -= 1
 
-print(result)
+print(''.join(result))
