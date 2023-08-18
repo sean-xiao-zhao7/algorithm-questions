513313c0a63f96ff32cac99b20455fbaace79bb0
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-08-18 14:26:19 -0400

    Use re/functools for anagram

--- anagram.py
+++ anagram.py
@@ -1,26 +1,20 @@
+import re
+import functools
+
 input1 = "Whoa! Hi!"
 input2 = "Hi! Whoa!"
 
 if input1 == input2:
     print(f'"{input1}" is an anagram of "{input2}"')
+    exit()
 
-input1_len = len(input1)
-input2_len = len(input2)
-max_len = input1_len if input1_len > input2_len else input2_len
+cleaned_input1 = re.sub(r'[^a-zA-Z0-9]', '', input1)
+cleaned_input2 = re.sub(r'[^a-zA-Z0-9]', '', input2)
 
-ord_total = 0
+ord1 = functools.reduce(lambda x, y: x + ord(y), cleaned_input1, 0)
+ord2 = functools.reduce(lambda x, y: x + ord(y), cleaned_input2, 0)
 
-for index in range(0, max_len, 1):
-    if index < input1_len:
-        char1 = input1[index]
-        if char1.isalnum():
-            ord_total += ord(char1.lower())
-    if index < input2_len:
-        char2 = input2[index]
-        if char2.isalnum():
-            ord_total -= ord(char2.lower())
-
-if ord_total == 0:
+if ord1 == ord2:
     print(f'"{input1}"\nis an anagram of\n"{input2}"')
 else:
     print(f'"{input1}"\nis not an anagram of\n"{input2}"')
