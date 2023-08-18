76608f1997e7ba62ff7dfb7f9964f203d7be7785
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-08-18 14:13:18 -0400

    Prettify output for anagram

--- anagram.py
+++ anagram.py
@@ -1,8 +1,8 @@
-input1 = 'Hi there'
-input2 = 'Bye there'
+input1 = "Whoa! Hi!"
+input2 = "Hi! Whoa!"
 
 if input1 == input2:
-    print(f'{input1} is an anagram of {input2}')
+    print(f'"{input1}" is an anagram of "{input2}"')
 
 input1_len = len(input1)
 input2_len = len(input2)
@@ -21,6 +21,6 @@ for index in range(0, max_len, 1):
             ord_total -= ord(char2.lower())
 
 if ord_total == 0:
-    print(f'{input1} is an anagram of {input2}')
+    print(f'"{input1}"\nis an anagram of\n"{input2}"')
 else:
-    print(f'{input1} is not an anagram of {input2}')
+    print(f'"{input1}"\nis not an anagram of\n"{input2}"')
