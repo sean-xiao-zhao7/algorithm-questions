23b6d71f625ed3861fd1c0fed282d4416ba718b7
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-08-18 14:51:39 -0400

    Add improved step

--- step.py
+++ step.py
@@ -1,7 +1,6 @@
-input = 10
+input = 5
 
+output = '#'
 for index in range(1, input + 1):
-    output = ''
-    for index2 in range(index):
-        output += '#'
     print(output)
+    output += '#'
