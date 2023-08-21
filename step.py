7a9ccca7169809ff42e7aead887fc4161538b3ee
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-08-21 16:23:38 -0400

    Implement step using recursion

--- step.py
+++ step.py
@@ -1,6 +1,24 @@
 input = 10
 
-output = '#'
-for index in range(1, input + 1):
-    print(">" + output + ' ' * (input - index) + "<")
-    output += '#'
+# output = '#'
+# for index in range(1, input + 1):
+#     print(">" + output + ' ' * (input - index) + "<")
+#     output += '#'
+
+
+def step(max, current=1):
+    """
+    Print a triangle of #s, padding by white space.
+
+    Parameters:
+    current (int): current number of #s to be printed.
+    max (int): the max number of #s in the whole program.
+    """
+    print(">" + "#" * current + " " * (max - current) + "<")
+    if (current == max):
+        exit()
+    else:
+        step(max, current + 1)
+
+
+step(10)
