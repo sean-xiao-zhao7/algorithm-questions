f5416bd3a2a0f57145ae6de3da8f156e86dc2935
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-08-23 14:49:40 -0400

    Add completed Q DS

--- ds/q.py
+++ ds/q.py
@@ -1,3 +1,30 @@
 class Queue:
     def __init__(self):
         self.queue = []
+
+    def add(self, input):
+        """
+        Add input to start of self queue.
+        Param:
+        input (int): the integer to be added.
+        """
+        self.queue.insert(0, input)
+
+    def remove(self):
+        """
+        Remove the last item from the queue.                
+        """
+        self.queue.pop()
+
+    def __repr__(self):
+        """
+        Print the queue for debugging.
+        """
+        return str(self.queue)
+
+
+if __name__ == '__main__':
+    q = Queue()
+    for item in range(10):
+        q.add(item)
+    print(q)
