2b3d8c0137851bf40c1f2d9a5c9b6fecc0d8e213
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-08-24 12:56:41 -0400

    Complete insertLast

--- ds/linked_list.py
+++ ds/linked_list.py
@@ -1,3 +1,4 @@
+import copy
 from linked_list_node import LinkedListNode
 
 
@@ -5,6 +6,7 @@ class LinkedList:
     def __init__(self) -> None:
         self._head = None
         self._size = 0
+        self._tail = None
 
     @property
     def head(self):
@@ -15,6 +17,14 @@ class LinkedList:
         self._head = node
 
     @property
+    def tail(self):
+        return self._tail
+
+    @tail.setter
+    def tail(self, node):
+        self._tail = node
+
+    @property
     def size(self):
         return self._size
 
@@ -23,14 +33,39 @@ class LinkedList:
         self._size = value
 
     def insertFirst(self, value):
+        """
+        Insert a single node at the beginning (left most) of the list.
+        """
         newNode = LinkedListNode(value)
         if not self.head:
             self.head = newNode
+            self.tail = self.head
         else:
             temp = self.head
             self.head = newNode
             self.head.next = temp
         self.size += 1
+
+    def insertLast(self, value):
+        """
+        Insert a single node at the end (right most) of the list.
+        """
+        if not self.head:
+            self.insertFirst(value)
+        else:
+            newNode = LinkedListNode(value)
+            self.tail.next = newNode
+            self.tail = newNode
+        self.size += 1
+
+    def getFirst(self):
+        """
+        Return a single node at the beginning (left most) of the list.
+        """
+        if self.head:
+            return self.head
+        else:
+            return False
 
     def __repr__(self) -> str:
         if not self.head:
@@ -48,4 +83,7 @@ if __name__ == '__main__':
     llist1 = LinkedList()
     llist1.insertFirst(1)
     llist1.insertFirst(2)
-    print(llist1.size)
+    llist1.insertLast(0)
+    llist1.insertLast(-1)
+    llist1.insertFirst(3)
+    print(llist1)
