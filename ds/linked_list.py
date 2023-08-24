2da00dbdeb5ae3590738029895da8c033f6afa2a
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-08-24 12:16:49 -0400

    Add size to LL

--- ds/linked_list.py
+++ ds/linked_list.py
@@ -4,6 +4,7 @@ from linked_list_node import LinkedListNode
 class LinkedList:
     def __init__(self) -> None:
         self._head = None
+        self._size = 0
 
     @property
     def head(self):
@@ -13,6 +14,14 @@ class LinkedList:
     def head(self, node):
         self._head = node
 
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        self._size = value
+
     def insertFirst(self, value):
         newNode = LinkedListNode(value)
         if not self.head:
@@ -21,6 +30,7 @@ class LinkedList:
             temp = self.head
             self.head = newNode
             self.head.next = temp
+        self.size += 1
 
     def __repr__(self) -> str:
         if not self.head:
@@ -38,4 +48,4 @@ if __name__ == '__main__':
     llist1 = LinkedList()
     llist1.insertFirst(1)
     llist1.insertFirst(2)
-    print(llist1)
+    print(llist1.size)
