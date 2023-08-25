1ebe66d591ac3c34e9d6be448d8ccc88cf3a0243
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-08-25 14:55:59 -0400

    Implement breadthFirstTraverse

--- ds/tree.py
+++ ds/tree.py
@@ -11,27 +11,39 @@ class Tree:
     def __init__(self, initialNodeValue) -> None:
         self.root = TreeNode(initialNodeValue)
 
-    def breadthFirstTraverse(self):
+    def depthFirstTraverse(self):
         """
-        Implement complete breadth first traversal of the tree.
+         Implement complete depth first traversal of the tree.
         """
-        Tree._breadthFirstTraverse_helper(self.root)
+        Tree._depthFirstTraverse_helper(self.root)
 
     @staticmethod
-    def _breadthFirstTraverse_helper(node, level=1):
+    def _depthFirstTraverse_helper(node, level=1):
         """
-        Recursive helper method for BFT.
+        Recursive helper method for DFT.
         """
         print(f'Level {level}: {node}')
         if node.children != {}:
             for childNode in node.children.values():
-                Tree._breadthFirstTraverse_helper(childNode, level + 1)
+                Tree._depthFirstTraverse_helper(childNode, level + 1)
 
-    def depthFirstTraverse(self):
+    def breadthFirstTraverse(self):
         """
-        Implement complete depth first traversal of the tree.
+        Implement complete breadth first traversal of the tree.
         """
-        pass
+        print(f'Level {1}: {self.root}')
+        Tree._breadthFirstTraverse_helper(self.root, 2)
+
+    @staticmethod
+    def _breadthFirstTraverse_helper(node, level=2):
+        """
+        Recursive helper method for BFT.
+        """
+        if node.children != {}:
+            for childNode in node.children.values():
+                print(f'Level {level}: {childNode}')
+            for childNode in node.children.values():
+                Tree._breadthFirstTraverse_helper(childNode, level + 1)
 
 
 if __name__ == '__main__':
