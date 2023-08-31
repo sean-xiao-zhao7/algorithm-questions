4e74074f212e04539c42ba91c81ddd931a9f9c73
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-08-31 14:34:13 -0400

    Re-start attempt at level width

--- level_width.py
+++ level_width.py
@@ -1,22 +1,15 @@
 from ds.tree import Tree
 
 
-def level_width(tree):
-    result = [1]
-    level_width_helper(tree.root.children.values(), result)
-    return result
-
-
-def level_width_helper(current_children, result):
+def level_width(current_node, widths):
     """
-    Recursively complete the list showing each tree level's children count.
+    Recursively return a dictionary that keys are level of the tree
+    and width of the corresponding level is the value.
+    Param:
+    current_node (TreeNode): the current TreeNode to use.
+    widths (Dict key(int): value(int)): dict that contains levels and corresponding widths
     """
-    count = 0
-    for child in current_children:
-        count += 1
-    result.insert(0, count)
-    for child in current_children:
-        level_width_helper(child.children.values(), result)
+    pass
 
 
 if __name__ == '__main__':
@@ -25,4 +18,5 @@ if __name__ == '__main__':
         tree.root.addChild(i)
         for j in range(5):
             tree.root.children[i].addChild(j)
-    print(level_width(tree))
+    print(tree.breadthFirstTraverse())
+    # print(level_width(tree))
