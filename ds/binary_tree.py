a12941d7a6575e94c181740b8faeb5862cab383e
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-08-31 15:06:42 -0400

    Add first implementation of BST

--- ds/binary_tree.py
+++ ds/binary_tree.py
@@ -1,8 +1,20 @@
-from tree import Tree
+from ds.tree import Tree
+from ds.bst_tree_node import BSTTreeNode
 
 
 class BinarySearchTree(Tree):
     """
     BST.
     """
-    pass
+
+    def __init__(self, initialNodeValue) -> None:
+        self.root = BSTTreeNode(initialNodeValue)
+
+
+if __name__ == '__main__':
+    tree = BinarySearchTree(0)
+    for i in range(1, 5):
+        tree.root.addChild(i)
+        for j in range(i, 5):
+            tree.root.children[i].addChild(j)
+    tree.breadthFirstTraverse()
