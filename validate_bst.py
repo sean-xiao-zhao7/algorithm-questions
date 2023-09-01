d2d66538ac5bb3a5b3e18448e2f8b9939863452c
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-01 15:03:33 -0400

    Add first attempt of validateBST

--- validate_bst.py
+++ validate_bst.py
@@ -1,7 +1,23 @@
 from ds.binary_search_tree import BinarySearchTree
 
+
+def validateBST(tree):
+    """
+    Return True if tree is true BST.
+    """
+    nodes_this_level = [tree.root]
+    while len(nodes_this_level) > 0:
+        for current_node in nodes_this_level:
+            if current_node.value < current_node.children['left'].value \
+                    or current_node.value > current_node.children['right'].value:
+                return False
+        nodes_this_level = []
+        for current_node in nodes_this_level:
+            nodes_this_level + current_node.children.values()
+
+
 if __name__ == '__main__':
     tree = BinarySearchTree(0)
     for i in range(10):
         tree.addChildBST(i)
-    tree.breadthFirstTraverse()
+    validateBST(tree)
