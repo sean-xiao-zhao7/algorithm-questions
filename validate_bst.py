c753f79e73768682bd85d46dc0f2883717ff7074
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-05 14:12:44 -0400

    Add min/max of validation

--- validate_bst.py
+++ validate_bst.py
@@ -50,10 +50,27 @@ def validateBST(current_node, left_parents=[], right_parents=[]):
     return result_left and result_right
 
 
+def validateBST2(current_node, current_min=None, current_max=None):
+    """
+    Validate BST using min/max way.    
+    """
+    if (current_min and current_node.value < current_min) or \
+            (current_max and current_node.value > current_max):
+        return False
+
+    if 'left' in current_node.children:
+        return validateBST2(current_node.children['left'], current_min, current_node.value)
+
+    if 'right' in current_node.children:
+        return validateBST2(current_node.children['right'], current_node.value, current_max)
+
+    return True
+
+
 if __name__ == '__main__':
     tree = BinarySearchTree(randint(1, 50))
     for i in range(1, 10):
         new_node_value = randint(1, 50)
         print(f'Inserting {new_node_value}')
         tree.addChildBST(new_node_value)
-    print(validateBST(tree.root))
+    print(validateBST2(tree.root))
