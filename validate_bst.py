06cd2d56ed8eb6584f3f37327834d5cf04ac4015
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-05 13:12:11 -0400

    Add new solution to BST verify

--- validate_bst.py
+++ validate_bst.py
@@ -1,20 +1,38 @@
 from ds.binary_search_tree import BinarySearchTree
 
 
-def validateBST(tree):
+def validateBST(current_node, left_parents=[], right_parents=[]):
     """
     Return True if tree is true BST.
     """
-    nodes_this_level = [tree.root]
-    while len(nodes_this_level) > 0:
-        for current_node in nodes_this_level:
-            if current_node.value < current_node.children['left'].value \
-                    or current_node.value > current_node.children['right'].value:
-                return False
-        nodes_this_level = []
-        for current_node in nodes_this_level:
-            nodes_this_level + current_node.children.values()
-    return True
+
+    # verify children adhere to BST
+    if 'left' in current_node.children and \
+            current_node.children['left'].value > current_node.value:
+        return False
+    if 'right' in current_node.children and \
+            current_node.children['right'].value < current_node.value:
+        return False
+
+    # verify current node adhere to BST in relation to all parents
+    if len(right_parents):
+        if not all(current_node.value <= element for element in right_parents):
+            return False
+    if len(left_parents):
+        if not all(current_node.value >= element for element in left_parents):
+            return False
+
+    # register current node in the parent history
+    if 'left' in current_node.children:
+        right_parents.append(current_node.value)
+        result_left = validateBST(
+            current_node.children['left'], left_parents, right_parents)
+    if 'right' in current_node.children:
+        left_parents.append(current_node.value)
+        result_right = validateBST(
+            current_node.children['right'], left_parents, right_parents)
+
+    return result_left and result_right
 
 
 if __name__ == '__main__':
