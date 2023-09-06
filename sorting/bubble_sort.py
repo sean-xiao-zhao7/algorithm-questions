1fa2a7eca3d0b74ced65b21828498aa3e830d9fa
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-06 15:12:58 -0400

    Complete bubble sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -4,9 +4,10 @@ input = [randint(0, 50) for num in range(10)]
 
 print('Original:', input, '\n')
 
+max = 0
 for i in range(len(input)):
-    for j in range(len(input) - i):
-        if input[i] < input[j]:
-            input[i], input[j] = input[j], input[i]
+    for j in range(len(input) - i - 1):
+        if input[j] > input[j + 1]:
+            input[j + 1], input[j] = input[j], input[j + 1]
 
 print('Sorted:', input)
