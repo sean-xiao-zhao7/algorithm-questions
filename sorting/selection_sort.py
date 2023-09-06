cdfc7c0d9c02e9dff59018ddd8a76866ae38f71c
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-06 17:40:31 -0400

    Add selection sort

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -4,10 +4,9 @@ input = [randint(0, 50) for num in range(100)]
 
 print('Original:', input, '\n')
 
-max = 0
 for i in range(len(input)):
-    for j in range(len(input) - i - 1):
-        if input[j] > input[j + 1]:
-            input[j + 1], input[j] = input[j], input[j + 1]
+    for j in range(i, len(input)):
+        if input[i] > input[j]:
+            input[i], input[j] = input[j], input[i]
 
 print('Sorted:', input)
