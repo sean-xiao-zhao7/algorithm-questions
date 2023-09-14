44bb991c01ac6ed3dfaf582ec062394e4627a7e3
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-14 13:43:21 -0400

    Complete find_two_target_sum

--- questions/find_sum.py
+++ questions/find_sum.py
@@ -8,23 +8,25 @@ def find_two_target_sum(integers, target_sum):
     integers (int[]): list of random integers in random order.
     target_sum (int): the target sum.
     """
-    print(f'Attempting to find for sum {target_sum}')
+    print(f'Attempting to find for sum {target_sum}.')
 
     # keep a record of unique integers seen so far
     seen = {}
 
     # loop through all items in the list
-    for integer in integers:
+    for index, integer in enumerate(integers):
+        print(f'Seen {integer}.')
+
         # save the current integer as key/value pair
         if integer not in seen:
             seen[integer] = integer
 
         # check if any saved integer sums up with current integer to target_sum
-        leftover = abs(target_sum - seen)
-        if seen[leftover]:
+        leftover = abs(target_sum - integer)
+        if leftover in seen:
             print(
-                f'Found {leftover} and {integer} that sums up to {target_sum}.')
-            exit
+                f'Found {leftover} and {integer} that sum up to {target_sum}.\nIt took {index + 1} iterations.')
+            exit()
     print(
         f'Could not find two integers that sums up to {target_sum}.')
 
