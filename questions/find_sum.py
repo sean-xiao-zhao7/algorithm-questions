7e0d688551ec82d7a3f46387cf8551dfede4ce7d
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-14 14:11:01 -0400

    Fix find_two_target_sum
    Program was saving integer in seen before checking for sum.
    This resulted in issue where the saved integer is summed with itself.

--- questions/find_sum.py
+++ questions/find_sum.py
@@ -1,4 +1,5 @@
 from random import randint
+from timeit import timeit
 
 
 def find_two_target_sum(integers, target_sum):
@@ -17,20 +18,52 @@ def find_two_target_sum(integers, target_sum):
     for index, integer in enumerate(integers):
         print(f'Seen {integer}.')
 
+        # check if any saved integer sums up with current integer to target_sum
+        leftover = abs(target_sum - integer)
+        if leftover in seen:
+            print(
+                f'Found {leftover} and {integer} that sum up to {target_sum}.\nIt took {index + 1} iterations.')
+            return
+
         # save the current integer as key/value pair
         if integer not in seen:
             seen[integer] = integer
+    print(
+        f'Could not find two integers that sums up to {target_sum}.')
+
+
+def find_two_target_sum_ordered(integers, target_sum):
+    """
+    Find 2 integers from the given list that sum up to target_sum.
+    Params:
+    integers (int[]): list of random integers in ascending order.
+    target_sum (int): the target sum.
+    """
+    print(f'Attempting to find for sum {target_sum}.')
+
+    # keep a record of unique integers seen so far
+    seen = {}
+
+    # loop through all items in the list
+    for index, integer in enumerate(integers):
+        print(f'Seen {integer}.')
 
         # check if any saved integer sums up with current integer to target_sum
         leftover = abs(target_sum - integer)
         if leftover in seen:
             print(
                 f'Found {leftover} and {integer} that sum up to {target_sum}.\nIt took {index + 1} iterations.')
-            exit()
+            return
+
+        # save the current integer as key/value pair
+        if integer not in seen:
+            seen[integer] = integer
     print(
         f'Could not find two integers that sums up to {target_sum}.')
 
 
 if __name__ == '__main__':
-    find_two_target_sum([randint(1, 100)
-                        for dummy in range(100)], randint(2, 198))
+    random_max = randint(100, 200)
+    execution_time = timeit(
+        "find_two_target_sum_ordered([current_integer for current_integer in range(1, random_max)], randint(2, random_max * 2 - 2))", globals=globals(), number=1)
+    print(f"Execution time: {execution_time:.6f} seconds")
