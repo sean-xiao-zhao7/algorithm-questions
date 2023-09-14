c8654b5d7ef3558b22a65d73fa37b6aeca834859
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-14 15:16:55 -0400

    Add first attempt of common_items

--- questions/common_items.py
+++ questions/common_items.py
@@ -21,10 +21,20 @@ def common_items(list1, list2):
     """
     Return bool indicating whether both lists contain at least 1 common item.
     Params:
-    list1 (String[]): list of strings #1.
-    list2 (String[]): list of strings #2.
+    list1 (String[]): list of strings #1 of random values and order.
+    list2 (String[]): list of strings #2 of random values and order.
     """
-    pass
+    # keep a hash map of unique strings from list1, for quick look up later
+    seen = {}
+    for string in list1:
+        if string not in seen:
+            seen[string] = None
+
+    # iterate through list2, test each item against seen.
+    for string in list2:
+        if string in seen:
+            return True
+    return False
 
 
 list1 = [make_random_string(10, 20) for index in range(20)]
