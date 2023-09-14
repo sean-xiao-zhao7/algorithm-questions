1dd0643c531b0bf197ea702062f0b0373be5b1b8
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-14 14:39:50 -0400

    Improve find_two_target_sum
    Do not attempt to check if current iteration's integer > target_sum

--- questions/find_sum.py
+++ questions/find_sum.py
@@ -16,36 +16,9 @@ def find_two_target_sum(integers, target_sum):
 
     # loop through all items in the list
     for index, integer in enumerate(integers):
-        print(f'Seen {integer}.')
+        if integer > target_sum:
+            continue
 
-        # check if any saved integer sums up with current integer to target_sum
-        leftover = abs(target_sum - integer)
-        if leftover in seen:
-            print(
-                f'Found {leftover} and {integer} that sum up to {target_sum}.\nIt took {index + 1} iterations.')
-            return
-
-        # save the current integer as key/value pair
-        if integer not in seen:
-            seen[integer] = integer
-    print(
-        f'Could not find two integers that sums up to {target_sum}.')
-
-
-def find_two_target_sum_ordered(integers, target_sum):
-    """
-    Find 2 integers from the given list that sum up to target_sum.
-    Params:
-    integers (int[]): list of random integers in ascending order.
-    target_sum (int): the target sum.
-    """
-    print(f'Attempting to find for sum {target_sum}.')
-
-    # keep a record of unique integers seen so far
-    seen = {}
-
-    # loop through all items in the list
-    for index, integer in enumerate(integers):
         print(f'Seen {integer}.')
 
         # check if any saved integer sums up with current integer to target_sum
@@ -65,5 +38,5 @@ def find_two_target_sum_ordered(integers, target_sum):
 if __name__ == '__main__':
     random_max = randint(100, 200)
     execution_time = timeit(
-        "find_two_target_sum_ordered([current_integer for current_integer in range(1, random_max)], randint(2, random_max * 2 - 2))", globals=globals(), number=1)
+        "find_two_target_sum([randint(1, random_max) for current_integer in range(1, random_max)], randint(2, random_max * 2 - 2))", globals=globals(), number=1)
     print(f"Execution time: {execution_time:.6f} seconds")
