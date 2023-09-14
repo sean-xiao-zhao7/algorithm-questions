6abcfb9bd3e44db8844f585c53c4200c880d58e9
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-14 13:39:14 -0400

    Add first attempt of find_two_target_sum

--- questions/find_sum.py
+++ questions/find_sum.py
@@ -10,6 +10,24 @@ def find_two_target_sum(integers, target_sum):
     """
     print(f'Attempting to find for sum {target_sum}')
 
+    # keep a record of unique integers seen so far
+    seen = {}
+
+    # loop through all items in the list
+    for integer in integers:
+        # save the current integer as key/value pair
+        if integer not in seen:
+            seen[integer] = integer
+
+        # check if any saved integer sums up with current integer to target_sum
+        leftover = abs(target_sum - seen)
+        if seen[leftover]:
+            print(
+                f'Found {leftover} and {integer} that sums up to {target_sum}.')
+            exit
+    print(
+        f'Could not find two integers that sums up to {target_sum}.')
+
 
 if __name__ == '__main__':
     find_two_target_sum([randint(1, 100)
