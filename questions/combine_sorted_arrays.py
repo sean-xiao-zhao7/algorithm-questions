f73e73ec21046c16f86cc999a763cd9b6f5f3383
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-18 15:24:24 -0400

    Add make_random_int_list

---
+++ questions/combine_sorted_arrays.py
@@ -0,0 +1,19 @@
+from utils.random import make_random_int_list
+
+
+def combine_sorted_lists(list1, list2):
+    """
+    Combine 2 sorted ascending integer lists into 1 sorted list.
+    Params:
+    list1 (list[int]): ascending sorted list of integers.
+    list2 (list[int]): ascending sorted list of integers.
+    """
+    pass
+
+
+if __name__ == '__main___':
+    input = make_random_int_list(100, 100)
+    input.sort()
+    input2 = make_random_int_list(100, 100)
+    input2.sort()
+    combine_sorted_lists(list1, list2)
--- utils/random.py
+++ utils/random.py
@@ -17,3 +17,16 @@ def make_random_string(min_len, max_len):
     for index in range(random.randint(min_len, max_len)):
         current_string += random.choice(characters)
     return current_string
+
+
+def make_random_int_list(length, max_range):
+    """
+    Generate a string of random integers between min_len and max_len length.
+    Params:
+    min_len (int): minimum length of the list.
+    max_len (int): maximum length of the list.
+    """
+    current_list = []
+    for index in range(length):
+        current_list.append(random.randint(0, max_range))
+    return current_list
