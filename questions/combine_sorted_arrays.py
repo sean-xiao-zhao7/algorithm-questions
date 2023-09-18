64dbc9dfba31c322b686370dc9d2c48758efdae2
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-18 15:49:23 -0400

    Fix comparison error

--- questions/combine_sorted_arrays.py
+++ questions/combine_sorted_arrays.py
@@ -8,12 +8,35 @@ def combine_sorted_lists(list1, list2):
     list1 (list[int]): ascending sorted list of integers.
     list2 (list[int]): ascending sorted list of integers.
     """
-    pass
+    result = []
+    current_list1_item = 0
+    current_list2_item = 0
+    while current_list1_item < len(list1) or current_list2_item < len(list2):
+        if current_list1_item == len(list1):
+            result.append(list2[current_list2_item])
+            current_list2_item += 1
+        elif current_list2_item == len(list2):
+            result.append(list1[current_list1_item])
+            current_list1_item += 1
+        else:
+            item1 = list1[current_list1_item]
+            item2 = list2[current_list2_item]
+            if item1 < item2:
+                result.append(item1)
+                current_list1_item += 1
+            else:
+                result.append(item2)
+                current_list2_item += 1
+
+    return result
 
 
-if __name__ == '__main___':
-    input = make_random_int_list(100, 100)
-    input.sort()
-    input2 = make_random_int_list(100, 100)
-    input2.sort()
-    combine_sorted_lists(list1, list2)
+if __name__ == '__main__':
+    list1 = make_random_int_list(100, 100)
+    list1.sort()
+    list2 = make_random_int_list(100, 100)
+    list2.sort()
+    print(list1)
+    print(list2)
+    result = combine_sorted_lists(list1, list2)
+    print('\n', result)
