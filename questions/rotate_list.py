a61ab7b8be6bba932c3521a748657621f4214cb8
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-19 15:06:40 -0400

    Start rotate list

---
+++ questions/rotate_list.py
@@ -0,0 +1,14 @@
+from utils.random import make_random_int_list
+
+
+def rotate_list_mine(nums, k):
+    """
+    Given an integer list nums, 
+    rotate the array to the right by k steps, where k is non-negative.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    nums = make_random_int_list(10, 20)
+    rotate_list_mine(nums)
