c878f885ed9a3f2f3269fdf4b6077e981df2e9a1
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-19 13:49:02 -0400

    Add find_largest_subarray_kadane

--- questions/find_largest_subarray.py
+++ questions/find_largest_subarray.py
@@ -1,4 +1,5 @@
 from utils.random import make_random_int_list
+from timeit import timeit
 
 
 def find_largest_subarray_brute(input_list):
@@ -17,16 +18,27 @@ def find_largest_subarray_brute(input_list):
     return largest_sum
 
 
-def find_largest_subarray(input_list):
+def find_largest_subarray_kadane(input_list):
     """
     Find the largest contiguous subarray from input_list.
     Params:
     input_list (list[int]): list of integers, both positive and negative.
     """
-    pass
+    largest_sum = 0
+    current_largest = 0
+    for item in input_list:
+        current_largest = current_largest + item
+        if current_largest < 0:
+            current_largest = 0
+        elif current_largest > largest_sum:
+            largest_sum = current_largest
+    return largest_sum
 
 
 if __name__ == '__main__':
-    test = make_random_int_list(10, 20, True)
-    print(test)
-    print(find_largest_subarray_brute(test))
+    test = make_random_int_list(1000, 20, True)
+    # print(test)
+    print(find_largest_subarray_kadane([5, 4, -1, 7, 8]))
+    # time = timeit(
+    #     "find_largest_subarray_brute(test)", globals=globals(), number=1)
+    # print(f'Used {time:.6f} seconds.')
