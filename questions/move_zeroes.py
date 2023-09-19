c98cc279727dbcbc75bbd57c13611067e2d4d105
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-19 14:48:16 -0400

    Add first attempt of move_zeroes_append

--- questions/move_zeroes.py
+++ questions/move_zeroes.py
@@ -12,8 +12,24 @@ def move_zeroes_mine(input_list):
             input_list.pop(index)
 
 
+def move_zeroes_append(input_list):
+    """
+    Given an integer array nums, move all 0's to the end of it - in place -
+    while maintaining the relative order of the non-zero elements.
+    """
+    index = 0
+    for item in input_list:
+        if item == 0:
+            continue
+        else:
+            input_list[index] = item
+            index += 1
+    return input_list + [0] * (len(input_list) - index)
+
+
 if __name__ == '__main__':
     test = make_random_int_list(1000, 20, True)
-    # print(test)
-    move_zeroes_mine(test)
-    # print('\n', test)
+    print(test)
+    move_zeroes_append(test)
+    # move_zeroes_mine(test)
+    print('\n', test)
