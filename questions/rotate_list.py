42a72b09c9f76fdcb16702f0cf2c02998e691a32
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-19 15:20:32 -0400

    Complete my solution for rotate_list

--- questions/rotate_list.py
+++ questions/rotate_list.py
@@ -7,16 +7,17 @@ def rotate_list_mine(nums, k):
     rotate the array to the right by k steps, where k is non-negative.
     """
     len_nums = len(nums)
-    new_nums = []
+    new_nums = [0] * len_nums
     for index, item in enumerate(nums):
         new_index = index + k
-        if new_index > len_nums:
-            new_index = len_nums - new_index
+        if new_index >= len_nums:
+            new_index = new_index - len_nums
         new_nums[new_index] = item
+    return new_nums
 
 
 if __name__ == '__main__':
     nums = make_random_int_list(10, 20)
     print(nums)
-    result = rotate_list_mine(nums)
+    result = rotate_list_mine(nums, 3)
     print(result)
