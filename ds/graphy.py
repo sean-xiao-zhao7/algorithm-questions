5b44bfe9bdddc1eacf063b47d390fa8dbaf708c4
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-22 14:20:56 -0400

    Add addEdge to graph

--- ds/graphy.py
+++ ds/graphy.py
@@ -2,6 +2,15 @@ class Graph:
     def __init__(self) -> None:
         self.graph = []
 
+    def addEdge(self, node1, node2):
+        """
+        Add an edge connected to existing edges.
+        Params:
+        edge (list[int]): list of 2 items, each integer, representing 2 nodes to be added.
+        """
+        self.graph.append([node1, node2])
+
 
 if __name__ == '__main__':
     graph = Graph()
+    graph.append([0, 1])
