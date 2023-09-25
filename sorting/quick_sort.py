2099dcdc5b5f737d4f0680cc688a4748eb58237e
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-25 15:03:00 -0400

    Add quicksort

---
+++ sorting/quick_sort.py
@@ -0,0 +1,26 @@
+from utils.random import make_random_int_list
+
+
+def quick_sort(list):
+    list_len = len(list)
+
+    # take pivot as last item
+    pivot_index = list_len - 1
+    pivot = list[pivot_index]
+
+    for index in range(list_len):
+        if list[index] > pivot:
+            # swap pivot with the item before it to make space
+            list[pivot_index - 1], list[pivot_index] = \
+                list[pivot_index], list[pivot_index - 1]
+            # swap list[index] to new space
+            list[index], list[pivot_index] = list[pivot_index], list[index]
+            # update pivot index
+            pivot_index = pivot_index - 1
+
+
+if __name__ == '__main__':
+    input = make_random_int_list(20, 100, True)
+    print(input, '\n')
+    result = quick_sort(input)
+    print(result)
