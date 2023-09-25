f94a48a42880ff8d6e022f37a2266f932ece35f1
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-25 16:08:00 -0400

    More attempts at quicksort

--- sorting/quick_sort.py
+++ sorting/quick_sort.py
@@ -4,33 +4,32 @@ from utils.random import make_random_int_list
 def quick_sort(list):
     list_len = len(list)
 
-    if list_len <= 1:
+    if list_len <= 2:
         return
 
     # take pivot as last item
     pivot_index = list_len - 1
     pivot = list[pivot_index]
-    print(f'Pivot is {pivot}')
-
+    print(list)
     current_index = 0
     while current_index < pivot_index:
         if list[current_index] >= pivot:
-            print(f'{list[current_index]} is greater than pivot.')
             # swap pivot with the item before it to make space
             list[pivot_index - 1], list[pivot_index] = \
                 list[pivot_index], list[pivot_index - 1]
-            print(list)
 
-            # swap list[index] to new space
-            list[current_index], list[pivot_index] = list[pivot_index], list[current_index]
-            print(list)
+            if list_len > 2:
+                # swap list[index] to new space
+                list[current_index], list[pivot_index] = list[pivot_index], list[current_index]
 
             # update pivot index
             pivot_index = pivot_index - 1
             pivot = list[pivot_index]
-
         current_index += 1
 
+    print(pivot)
+    print(list[:pivot_index])
+    print(list[pivot_index + 1:], '\n')
     quick_sort(list[:pivot_index])
     quick_sort(list[pivot_index:])
 
@@ -38,5 +37,5 @@ def quick_sort(list):
 if __name__ == '__main__':
     input = make_random_int_list(20, 10, True)
     print(input, '\n')
-    result = quick_sort(input)
-    print(result)
+    quick_sort(input)
+    print(input)
