88d029752444277fece616cc7146967733cb786d
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2023-09-27 13:21:36 -0400

    Modulize selection_sort

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,12 +1,15 @@
 from random import randint
 
-input = [randint(0, 100) for num in range(100)]
 
-print('Original:', input, '\n')
+def selection_sort(input):
+    for i in range(len(input)):
+        for j in range(i, len(input)):
+            if input[i] > input[j]:
+                input[i], input[j] = input[j], input[i]
 
-for i in range(len(input)):
-    for j in range(i, len(input)):
-        if input[i] > input[j]:
-            input[i], input[j] = input[j], input[i]
 
-print('Sorted:', input)
+if __name__ == '__main__':
+    input = [randint(0, 100) for num in range(100)]
+    print('Original:', input, '\n')
+    selection_sort(input)
+    print('Sorted:', input)
--- sorting/test.py
+++ sorting/test.py
@@ -1,12 +1,15 @@
 from sorting.quick_sort import quick_sort
 from sorting.bubble_sort import bubble_sort
+from sorting.merge_sort import merge_sort
 from utils.random import make_random_int_list
 from timeit import timeit
 
 if __name__ == "__main__":
-    input = make_random_int_list(1000, 100, True)
+    input = make_random_int_list(10000, 100, True)
     print(input, '\n')
     result = timeit("quick_sort(input)", globals=globals(), number=1)
     print(f'QS: {result:.6f} secs.')
-    result2 = timeit("bubble_sort(input)", globals=globals(), number=1)
-    print(f'BS: {result2:.6f} secs.')
+    result2 = timeit("merge_sort(input)", globals=globals(), number=1)
+    print(f'MS: {result2:.6f} secs.')
+    result3 = timeit("bubble_sort(input)", globals=globals(), number=1)
+    print(f'BS: {result3:.6f} secs.')
