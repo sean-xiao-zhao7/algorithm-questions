ac5b81592eb54a5653dc3d6e7c7da0ea53c1f450
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-01-12 16:49:57 -0500

    Add sorting as ESM

--- sorting/bubble_sort.js
+++ sorting/bubble_sort.js
@@ -18,7 +18,4 @@ const bubbleSort = (nums) => {
     }
 };
 
-const nums = [11212, 2324, 12, 123, 5343, 123];
-console.log(nums);
-bubbleSort(nums);
-console.log(nums);
+export default bubbleSort;
---
+++ sorting/test.js
@@ -0,0 +1,6 @@
+import bubbleSort from "./bubble_sort.js";
+
+const nums = [11212, 2324, 12, 123, 5343, 123];
+console.log(nums);
+bubbleSort(nums);
+console.log(nums);
