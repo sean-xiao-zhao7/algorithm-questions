6bb1dd030ee5d8732e1f50122f0b8b936c2286f6
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-12 14:56:07 -0400

    Add decode pyramid

---
+++ questions/decode_pyramid.py
@@ -0,0 +1,7 @@
+
+def decode(message_file):
+    pass
+
+if __name__ == '__main__':
+    result = decode('./inputs/decode_pyramid/0.txt')
+    print(result)
--- sorting/test.js
+++ sorting/test.js
@@ -1,6 +1,15 @@
 import bubbleSort from "./bubble_sort.js";
 import insertionSort from "./insertion_sort.js";
 
+const generateDataset = (length) => {
+    const result = [];
+    let index = 0;
+    while (index < length) {
+        result.push(Math.random() * 10);
+    }
+    return result;
+};
+
 let nums = [11212, 2324, 12, 123, 5343, 123];
 console.log(nums);
 // bubbleSort(nums);
