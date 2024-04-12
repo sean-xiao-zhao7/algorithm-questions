00120a183fde6a105da6e6433f66177ab8b49b96
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-12 15:22:48 -0400

    Add words construct in decode

--- questions/decode_pyramid.py
+++ questions/decode_pyramid.py
@@ -1,9 +1,14 @@
 
 def decode(message_file):
     file = open(message_file, 'r')
+    
     line = file.readline()
+    words = {}
     while line:
-        pass
+        parts = line.split(' ')        
+        words[parts[0]] = parts[1].strip('\n')
+        line = file.readline()
+    print(words)
     file.close()
     
 if __name__ == '__main__':
