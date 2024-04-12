a75276493c1dc4632d0f2a835ddd933ce3838697
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-04-12 15:02:12 -0400

    Add base decode

--- questions/decode_pyramid.py
+++ questions/decode_pyramid.py
@@ -1,7 +1,11 @@
 
 def decode(message_file):
-    pass
-
+    file = open(message_file, 'r')
+    line = file.readline()
+    while line:
+        pass
+    file.close()
+    
 if __name__ == '__main__':
     result = decode('./inputs/decode_pyramid/0.txt')
     print(result)
