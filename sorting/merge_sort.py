1cd3445185d9da9db6f25dd92e09552f5da67d38
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-10-01 19:34:02 -0400

    Add new merge_sort

--- sorting/merge_sort.py
+++ sorting/merge_sort.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-def merge_sort(input):
+def merge_sortOLD(input):
     """
     Implement the merge sort algorithm.
     Params:
@@ -16,7 +16,7 @@ def merge_sort(input):
         return _merge(merge_sort(input[:size // 2]), merge_sort(input[size // 2:]))
 
 
-def _merge(list1, list2):
+def _mergeOLD(list1, list2):
     """
     Merge the sorted sublists from merge sort.
     """
@@ -44,8 +44,36 @@ def _merge(list1, list2):
     return result
 
 
+def merge_sort(list):
+    """
+    Merge 2 sorted lists into 1.
+    """
+    first_half = list[0:len(list)]
+    second_half = list[len(list):-1]
+    result = _merge(first_half, second_half)
+    return result
+
+
+def _merge(list1, list2):
+    """
+    Merge 2 sorted lists into 1.
+    """
+    result = []
+    index1 = 0
+    index2 = 0
+    while index1 < len(list1):
+        while index2 < len(list2):
+            if list[index1] <= list[index2]:
+                result.append(list[index1])
+            else:
+                result.append(list[index2])
+            index2 += 1
+        index1 += 1
+    return result
+
+
 if __name__ == "__main__":
-    input = [randint(0, 100) for num in range(100)]
+    input = [randint(0, 10) for num in range(10)]
     print('Original:', input, '\n')
     result = merge_sort(input)
     print('Sorted:', result)
