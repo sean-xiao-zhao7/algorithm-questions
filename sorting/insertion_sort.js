5d5f58cfc5203808c9a9f8604b1d49a981241bcf
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-11-15 14:26:28 -0500

    Fix insertion sort JS

--- sorting/insertion_sort.js
+++ sorting/insertion_sort.js
@@ -3,14 +3,20 @@ const insertionSort = (input) => {
     for (const [currentInputIndex, currentInputValue] of Object.entries(
         input
     )) {
+        let inserted = false;
         for (const [currentSortedIndex, currentSortedValue] of Object.entries(
             sorted
         )) {
             if (currentSortedValue > currentInputValue) {
-                sorted.splice(currentSortedIndex, 0, currentNum);
+                sorted.splice(currentSortedIndex, 0, currentInputValue);
+                inserted = true;
                 break;
             }
         }
+        if (!inserted) {
+            sorted.push(currentInputValue);
+        }
+        inserted = false;
     }
     return sorted;
 };
