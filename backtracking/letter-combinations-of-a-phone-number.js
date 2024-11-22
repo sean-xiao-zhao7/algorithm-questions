19250c24ddf20e73a89c00a15ad96dcb65c613a7
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-11-22 15:52:19 -0500

    Add base letterCombinations

--- backtracking/letter-combinations-of-a-phone-number.js
+++ backtracking/letter-combinations-of-a-phone-number.js
@@ -2,6 +2,22 @@
  * @param {string} digits
  * @return {string[]}
  */
-const letterCombinations = function (digits) {};
+const letterCombinations = function (digits) {
+    const result = [];
+    for (let digit of digits) {
+    }
+    return result;
+};
+
+const numbers = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tub",
+    9: "wxyz",
+};
 
 export default letterCombinations;
--- test_javascript.js
+++ test_javascript.js
@@ -1,4 +1,5 @@
 import letterCombinationsOfAPhoneNumber from "./backtracking/letter-combinations-of-a-phone-number.js";
 
 const digits = "23";
-letterCombinationsOfAPhoneNumber(digits);
+const result = letterCombinationsOfAPhoneNumber(digits);
+console.log(result);
