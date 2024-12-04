5d267c26923f28f386a2d0693e5e5fce9fe1e811
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-12-04 15:03:57 -0500

    Add findMedianSortedArrays

--- algorithms/arrays/findMedianSortedArrays.js
+++ algorithms/arrays/findMedianSortedArrays.js
@@ -8,3 +8,10 @@ export const findMedianSortedArrays = (nums1, nums2) => {
     const len1 = nums1.length;
     const len2 = nums2.length;
 };
+
+export function generateTestInput(size) {
+    const finalInput1 = [1, 3, 5, 6, 7, 8, 10];
+    const finalInput2 = [2, 3, 4, 5, 6, 7, 8, 9, 12, 15];
+    // [1, 2, 3, 3, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 10, 12, 15]
+    return finalInput1, finalInput2;
+}
