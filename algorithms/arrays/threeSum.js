5a766830dcffb90b8e014340494b8fef7355ef20
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-12-19 17:49:58 -0500

    Add after threeSum

--- algorithms/arrays/threeSum.js
+++ algorithms/arrays/threeSum.js
@@ -10,13 +10,32 @@
 
 export function threeSum(nums) {
     const result = [];
+    const seen = {};
     for (let index = 0; index < nums.length; index++) {
+        if (nums[index] in seen) {
+            continue;
+        } else {
+            seen[nums[index]] = true;
+        }
         const twoSum = -nums[index];
+        const seen2 = {};
         for (let index2 = index + 1; index2 < nums.length; index2++) {
             const target = twoSum - nums[index2];
+            if (nums[index2] in seen2 || target in seen2) {
+                continue;
+            } else {
+                seen2[nums[index2]] = true;
+                seen2[target] = true;
+            }
             const targetIndex = nums.indexOf(target);
             if (targetIndex > -1) {
-                result.push([nums[index], nums[index2], nums[targetIndex]]);
+                const threeSumSet = [
+                    nums[index],
+                    nums[index2],
+                    nums[targetIndex],
+                ];
+                seen[threeSumSet] = threeSumSet;
+                result.push(threeSumSet);
             }
         }
     }
