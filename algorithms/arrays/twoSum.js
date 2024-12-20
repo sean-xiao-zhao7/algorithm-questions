fbe990ba3ead490d6dbe49ad9652eb76d9f9f664
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-12-19 19:11:17 -0500

    Retract previous 3 branching solutions

--- algorithms/arrays/twoSum.js
+++ algorithms/arrays/twoSum.js
@@ -4,16 +4,6 @@
  * @return {number[]}
  */
 export default function (numbers, target) {
-    if (numbers[0] < 0 && numbers[-1] > 0) {
-        return oneNegOnePos(numbers, target);
-    } else if (numbers[0] < 0 && numbers[-1] < 0) {
-        return bothNegs(numbers, target);
-    } else {
-        return bothPos(numbers, target);
-    }
-}
-
-function bothPos(numbers, target) {
     let overallIndex = 0,
         index1 = 0,
         index2 = numbers.length - 1;
@@ -29,71 +19,6 @@ function bothPos(numbers, target) {
 
         if (sum === target) {
             return [index1, index2];
-        }
-
-        if (sum < target) {
-        }
-
-        if (item2 > target) {
-            index2--;
-        }
-    }
-
-    return [index1, index2];
-}
-
-function oneNegOnePos(numbers, target) {
-    let overallIndex = 0,
-        index1 = 0,
-        index2 = numbers.length - 1;
-    while (overallIndex < numbers.length) {
-        const item1 = numbers[index1];
-        const item2 = numbers[index2];
-
-        if (item1 > target) {
-            return [-1, -1];
-        }
-
-        const sum = item1 + item2;
-
-        if (sum === target) {
-            return [index1, index2];
-        }
-
-        if (sum < target) {
-        }
-
-        if (item2 > target) {
-            index2--;
-        }
-    }
-
-    return [index1, index2];
-}
-
-function bothNegs(numbers, target) {
-    let overallIndex = 0,
-        index1 = 0,
-        index2 = numbers.length - 1;
-    while (overallIndex < numbers.length) {
-        const item1 = numbers[index1];
-        const item2 = numbers[index2];
-
-        if (item1 > target) {
-            return [-1, -1];
-        }
-
-        const sum = item1 + item2;
-
-        if (sum === target) {
-            return [index1, index2];
-        }
-
-        if (sum < target) {
-        }
-
-        if (item2 > target) {
-            index2--;
         }
     }
 
