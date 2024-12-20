f700e36e4b1ac2f16ed143f65478f53cd7375987
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-12-19 19:04:57 -0500

    Add twoSum base

--- algorithms/arrays/twoSum.js
+++ algorithms/arrays/twoSum.js
@@ -3,4 +3,99 @@
  * @param {number} target
  * @return {number[]}
  */
-export const twoSum = function (numbers, target) {};
+export default function (numbers, target) {
+    if (numbers[0] < 0 && numbers[-1] > 0) {
+        return oneNegOnePos(numbers, target);
+    } else if (numbers[0] < 0 && numbers[-1] < 0) {
+        return bothNegs(numbers, target);
+    } else {
+        return bothPos(numbers, target);
+    }
+}
+
+function bothPos(numbers, target) {
+    let overallIndex = 0,
+        index1 = 0,
+        index2 = numbers.length - 1;
+    while (overallIndex < numbers.length) {
+        const item1 = numbers[index1];
+        const item2 = numbers[index2];
+
+        if (item1 > target) {
+            return [-1, -1];
+        }
+
+        const sum = item1 + item2;
+
+        if (sum === target) {
+            return [index1, index2];
+        }
+
+        if (sum < target) {
+        }
+
+        if (item2 > target) {
+            index2--;
+        }
+    }
+
+    return [index1, index2];
+}
+
+function oneNegOnePos(numbers, target) {
+    let overallIndex = 0,
+        index1 = 0,
+        index2 = numbers.length - 1;
+    while (overallIndex < numbers.length) {
+        const item1 = numbers[index1];
+        const item2 = numbers[index2];
+
+        if (item1 > target) {
+            return [-1, -1];
+        }
+
+        const sum = item1 + item2;
+
+        if (sum === target) {
+            return [index1, index2];
+        }
+
+        if (sum < target) {
+        }
+
+        if (item2 > target) {
+            index2--;
+        }
+    }
+
+    return [index1, index2];
+}
+
+function bothNegs(numbers, target) {
+    let overallIndex = 0,
+        index1 = 0,
+        index2 = numbers.length - 1;
+    while (overallIndex < numbers.length) {
+        const item1 = numbers[index1];
+        const item2 = numbers[index2];
+
+        if (item1 > target) {
+            return [-1, -1];
+        }
+
+        const sum = item1 + item2;
+
+        if (sum === target) {
+            return [index1, index2];
+        }
+
+        if (sum < target) {
+        }
+
+        if (item2 > target) {
+            index2--;
+        }
+    }
+
+    return [index1, index2];
+}
--- test_javascript.js
+++ test_javascript.js
@@ -1,15 +1,15 @@
-import { mainFunc } from "./algorithms/arrays/twoSum.js";
+import mainFunc from "./algorithms/arrays/twoSum.js";
 
 export function generateInput(length, max) {
     const a = Array.from(new Array(length), (_) =>
         Math.floor(Math.random() * max * (Math.round(Math.random()) * 2 - 1))
     );
 
-    return a;
+    return a.sort((a, b) => (a > b ? 1 : -1));
 }
 
-const input = generateInput(10, 5);
+const input = generateInput(10, 10);
 console.log(input);
 
-const result = mainFunc(input);
+const result = mainFunc([2, 7, 11, 15], 9);
 console.log(result);
