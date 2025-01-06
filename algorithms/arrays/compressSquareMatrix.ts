36bc3a43dff275a5e3cb4a59ddb8619ca3e523d0
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-01-06 13:11:25 -0500

    Add findXMax helper
    Use this helper to deterimine the "x" number of maxes in a row of a matrix.

--- algorithms/arrays/compressSquareMatrix.ts
+++ algorithms/arrays/compressSquareMatrix.ts
@@ -3,14 +3,36 @@ export default function compressSquareMatrix(
     factors: number[],
     x: number
 ) {
-    console.log(matrix);
-    console.log(factors);
+    // console.log(matrix);
+    // console.log(factors);
 
     let sum = -1;
-    for (const row of matrix) {
+    let numsSelected = 0;
+    const numsSeen = {};
+
+    for (const row in matrix) {
         // console.log(row);
+        if (factors[row] <= 0) {
+            continue;
+        }
+
+        numsSeen[row] = findXMax(matrix[row], factors[row]);
     }
+
     return sum;
+}
+
+function findXMax(row: number[], factor: number) {
+    const xMaxArray: number[] = new Array(factor);
+    let max = 0,
+        maxIndex = 0;
+    for (const item in row) {
+        if (row[item] > max) {
+            max = row[item];
+            maxIndex = +item;
+            xMaxArray;
+        }
+    }
 }
 
 export function generateInputSquareMatrix(rows: number, valMax: number) {
