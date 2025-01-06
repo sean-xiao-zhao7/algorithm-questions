1aa886027091f0649585adbc171bbb97723ffa99
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-01-06 12:32:36 -0500

    Add generateInput2DArray

--- algorithms/arrays/compressSquareMatrix.ts
+++ algorithms/arrays/compressSquareMatrix.ts
@@ -2,4 +2,10 @@ export default function compressSquareMatrix(
     matrix: number[][],
     factors: number[],
     x: number
-) {}
+) {
+    let sum = -1;
+    for (const row of matrix) {
+        console.log(row);
+    }
+    return sum;
+}
--- test_javascript.ts
+++ test_javascript.ts
@@ -1,6 +1,6 @@
 import mainFunc from "./algorithms/arrays/compressSquareMatrix.js";
 
-export function generateInput(length, max) {
+export function generateInput1DArray(length, max) {
     const a = Array.from(new Array(length), (_) =>
         Math.floor(Math.random() * max * (Math.round(Math.random()) * 2 - 1))
     );
@@ -8,8 +8,22 @@ export function generateInput(length, max) {
     return a.sort((a, b) => (a > b ? 1 : -1));
 }
 
-const input = generateInput(10, 10);
+export function generateInput2DArray(
+    rows: number,
+    cols: number,
+    valMax: number
+) {
+    const main: number[][] = [];
+    for (let i = 0; i < rows; i++) {
+        let row: number[] = [];
+        for (let j = 0; j < cols; j++) {
+            row[j] = Math.floor(Math.random() * valMax);
+        }
+    }
+}
+
+const input = generateInput2DArray(5, 5, 10);
 console.log(input);
 
-// const result = mainFunc(input, 3);
+const result = mainFunc(input, 3);
 // console.log(result);
