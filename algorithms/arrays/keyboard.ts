9792bdf3057abae9036ba2154177760471f7dc33
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-01-07 15:24:09 -0500

    Add Keyboard

---
+++ algorithms/arrays/keyboard.ts
@@ -0,0 +1,9 @@
+/**
+ * Keyboard
+ * Return the number of steps needed to type out the searchTerm using keyboard.
+ * @param searchTerm
+ * @returns integer - the number of steps used to achieve the searchTerm.
+ */
+export default function Keyboard(searchTerm: string) {
+    const keyboard = "abcdefghijklmnopqrstuvwxy";
+}
