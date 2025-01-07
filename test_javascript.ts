2d7f697affda3d966ca52dfd5d6279cc90c7b64a
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-01-07 15:49:55 -0500

    Complete initial attempt of Keyboard

--- test_javascript.ts
+++ test_javascript.ts
@@ -1,6 +1,6 @@
 import mainFunc, { generateInput } from "./algorithms/arrays/keyboard.js";
 
-const input1 = generateInput(10);
+const input1 = generateInput(5);
 
 const result = mainFunc(input1);
 console.log(result);
