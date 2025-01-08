177381d909107ec9a84a3411b6bd353be92e5744
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-01-08 15:27:47 -0500

    Add partyChairs

---
+++ algorithms/arrays/partyChairs.ts
@@ -0,0 +1,15 @@
+/**
+ * partyChairs
+ *
+ * There are n guests who are invited to a party.
+ * The k-th guest will attend the party at time S[k] and leave the party at time E[k].
+ * Given an integer array S and an integer array E, both of length n,
+ *
+ * @param startTimes number[]
+ * @param endingTimes number[]
+ * @returns an integer denoting the minimum number of chairs you need such that everyone attending the party can sit down.
+ */
+export default function partyChairs(
+    startTimes: number[],
+    endingTimes: number[]
+) {}
