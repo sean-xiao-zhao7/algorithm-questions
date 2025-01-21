1cf4f044c374f117d59e1f4e3e0324dd38cb137e
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-01-21 18:05:37 -0500

    Complete stairsTabulationVersion

--- algorithms/math/stairs.ts
+++ algorithms/math/stairs.ts
@@ -1,11 +1,28 @@
 /**
  * Stairs
- *
+ * `stairsTabulationVersion` is a function that calculates the number of steps
+to climb `n` stairs using tabulation (bottom-up dynamic programming)
+approach. It iterates through the steps from 2 to `n`, calculating the
+number of ways to reach each step by summing up the ways to reach the
+previous two steps. Finally, it returns the total number of ways to reach
+the `n`-th step. 
  * @param {number} n
  * @returns {number} steps
  */
 
-export default function stairs(n: number) {
+export default function stairsTabulationVersion(n: number) {
+    let prev = 1,
+        prev2 = 1;
+    for (let i = 2; i < n + 1; i++) {
+        const current = prev + prev2;
+        prev2 = prev;
+        prev = current;
+    }
+
+    return prev;
+}
+
+const stairsRecursiveVersion = function (n: number) {
     if (n == 0) {
         return 1;
     }
@@ -13,5 +30,5 @@ export default function stairs(n: number) {
         return 1;
     }
 
-    return stairs(n - 1) + stairs(n - 2);
-}
+    return stairsRecursiveVersion(n - 1) + stairsRecursiveVersion(n - 2);
+};
