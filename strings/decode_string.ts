f0526857802b9a995174474a857be117e33f672d
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-01-21 15:01:57 -0500

    Add decode string

---
+++ strings/decode_string.ts
@@ -0,0 +1,20 @@
+/**
+ * Decode String
+ * https://leetcode.com/problems/decode-string/description/
+ * @param {string} encodedString
+ * @returns {string} decodedString
+ */
+
+export default function decodeString(encodedString: string) {
+    let decodedString = "";
+
+    return decodedString;
+}
+
+export function generateInput(length: number) {
+    let result = "";
+    for (let index = 0; index < length; index++) {
+        const randIndex = Math.floor(Math.random() * 25);
+    }
+    return result;
+}
