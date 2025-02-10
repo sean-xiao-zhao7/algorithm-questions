03221f287f3bf3798a5377c97a0beef19ff79f2f
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-02-10 13:28:41 -0500

    Add countPathsMatrixRecursive first attempt

--- algorithms/dp/countPathsMatrix.ts
+++ algorithms/dp/countPathsMatrix.ts
@@ -2,16 +2,42 @@
  * Count all possible paths from 0,0 to m-1,n-1 of a matrix of size m:n.
  * @param input integer 2D array.
  */
-const countPathsMatrixRecursive = function (input: number[][]) {};
 
-export function countPathsMatrixTab() {}
+const directions = {
+    vertical: "vertical",
+    horizontal: "horizontal",
+    diagonal: "diagonal",
+};
+
+const countPathsMatrixRecursive = function (
+    m: number,
+    n: number,
+    grandTotal: number
+) {
+    if (m == 0 && n == 0) {
+        return;
+    } else if (m == 0) {
+        return;
+    } else if (n == 0) {
+        return;
+    } else {
+        grandTotal += 2;
+        countPathsMatrixRecursive(m, n - 1, grandTotal);
+        countPathsMatrixRecursive(m - 1, n, grandTotal);
+        countPathsMatrixRecursive(m - 1, n - 1, grandTotal);
+    }
+};
+
+export function countPathsMatrixTab(m: number, n: number) {}
 
 function generateInput() {}
 
 export default function main() {
-    const input: number[][] = [];
+    // [(0,0), (0,1)
+    //  (1,0), (1,1)]
     const cache = [];
-    const currentResult = countPathsMatrixRecursive(input);
+    let grandTotal = 0;
+    const currentResult = countPathsMatrixRecursive(2, 2, grandTotal);
 
     return currentResult;
 }
