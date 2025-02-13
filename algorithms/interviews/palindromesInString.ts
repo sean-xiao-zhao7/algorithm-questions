e10d345cabfdcfd85da35bbb293ca5354ad05b16
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-02-13 17:12:53 -0500

    Add caching to countPalindromesInStringRecursive
    Don't calc same input string.

--- algorithms/interviews/palindromesInString.ts
+++ algorithms/interviews/palindromesInString.ts
@@ -10,18 +10,20 @@ function countPalindromesInStringRecursive(
     input: string,
     cache: { string: boolean } | {}
 ) {
-    //[a a b b a]
-    //[a] + [a, b, b, a]
-    //[a] + [a] + [b, b, a]
-    //[a] + [a] + [b] + [b, a]
-    //[a] + [a] + [b] + [b] + [a]
     const length = input.length;
     if (length == 1) {
         return 0;
     }
 
+    if (cache[input]) {
+        return 0;
+    } else {
+        cache[input] = true;
+    }
+
+    const currentResult: number = isPalidrome(input);
     const totalStartingThisChar =
-        1 +
+        currentResult +
         countPalindromesInStringRecursive(input.slice(0, length - 1), cache);
     const totalWithoutThisChar = countPalindromesInStringRecursive(
         input.slice(1, length),
@@ -31,8 +33,21 @@ function countPalindromesInStringRecursive(
     return totalStartingThisChar + totalWithoutThisChar;
 }
 
+function isPalidrome(input: string) {
+    const result = 1;
+    let start = 0,
+        end = input.length - 1;
+    while (start < end) {
+        if (input[start] !== input[end]) return 0;
+        start++;
+        end--;
+    }
+    console.log(input);
+    return result;
+}
+
 export default function main() {
-    const stringInput = "aaabba";
+    const stringInput = "aabba";
     return (
         stringInput.length + countPalindromesInStringRecursive(stringInput, {})
     );
