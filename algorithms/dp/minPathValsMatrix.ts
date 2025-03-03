ba2633a83bd77e04aa041a6c1b6675b25bc3fffd
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-03-03 16:11:42 -0500

    Add minPathValsMatrixRecursive first attempt

--- algorithms/dp/minPathValsMatrix.ts
+++ algorithms/dp/minPathValsMatrix.ts
@@ -1,14 +1,34 @@
 /**
  * Find path of min values. Each node in matrix has integer val > 0.
  *
- * @param input integer 2D array.
+ * @param matrix integer 2D array.
+ * @param m integer The width of the matrix
+ * @param n integer The height of the matrix
  * @returns minPathVal integer the value of the min path.
  */
 const minPathValsMatrixRecursive = function (
     m: number,
     n: number,
     matrix: number[][]
-) {};
+) {
+    const currentNodeVal = matrix[m - 1][n - 1];
+
+    // base case: only 1 node in the matrix, return the node's value as min
+    if (m === 1 && n === 1) return currentNodeVal;
+
+    let overallMin = 0;
+    if (m === 1) {
+        overallMin = minPathValsMatrixRecursive(m, n - 1, matrix);
+    } else if (n === 1) {
+        overallMin = minPathValsMatrixRecursive(m - 1, n, matrix);
+    } else {
+        const upMinVal = minPathValsMatrixRecursive(m, n - 1, matrix);
+        const leftMinVal = minPathValsMatrixRecursive(m - 1, n, matrix);
+        overallMin = Math.min(upMinVal, leftMinVal);
+    }
+
+    return currentNodeVal + overallMin;
+};
 
 export function minPathValsMatrixTab(m: number, n: number) {}
 
--- test_javascript.ts
+++ test_javascript.ts
@@ -1,4 +1,4 @@
-import main from "./algorithms/interviews/palindromesInString.ts";
+import main from "./algorithms/dp/minPathValsMatrix.ts";
 
 const result = main();
 console.log(result);
