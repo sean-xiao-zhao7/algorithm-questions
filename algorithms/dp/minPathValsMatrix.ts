96c34dc569d6581db4c929a3a9ed9f9a54e1b5d1
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-03-03 16:01:40 -0500

    Add minPathValsMatrix main code

--- algorithms/dp/minPathValsMatrix.ts
+++ algorithms/dp/minPathValsMatrix.ts
@@ -4,13 +4,27 @@
  * @param input integer 2D array.
  * @returns minPathVal integer the value of the min path.
  */
-const minPathValsMatrixRecursive = function (m: number, n: number) {};
+const minPathValsMatrixRecursive = function (
+    m: number,
+    n: number,
+    matrix: number[][]
+) {};
 
 export function minPathValsMatrixTab(m: number, n: number) {}
 
 function generateInput() {}
 
 export default function main() {
-    const minPathVal = minPathValsMatrixRecursive(3, 4);
+    const matrix = [
+        [1, 3, 4, 5],
+        [2, 5, 6, 7],
+        [1, 9, 6, 5],
+        [8, 8, 5, 7],
+    ];
+    const minPathVal = minPathValsMatrixRecursive(
+        matrix.length,
+        matrix[0].length,
+        matrix
+    );
     return minPathVal;
 }
