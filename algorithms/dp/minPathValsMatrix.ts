43457599cbe834d0bd59c18a463a3ff078e130b6
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-03-03 16:28:35 -0500

    Add minPathValsMatrixTab first attempt

--- algorithms/dp/minPathValsMatrix.ts
+++ algorithms/dp/minPathValsMatrix.ts
@@ -30,18 +30,39 @@ const minPathValsMatrixRecursive = function (
     return currentNodeVal + overallMin;
 };
 
-export function minPathValsMatrixTab(m: number, n: number) {}
+export function minPathValsMatrixTab(m: number, n: number, matrix: number[][]) {
+    const minVals: number[][] = [];
+    minVals[0][0] = matrix[0][0];
+    minVals[1][0] = matrix[0][0] + matrix[1][0];
+    minVals[0][1] = matrix[0][0] + matrix[0][1];
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            minVals[i][j] = Math.min(minVals[i - 1][j], minVals[i][j - 1]);
+        }
+    }
+    return minVals[m - 1][n - 1];
+}
 
 function generateInput() {}
 
 export default function main() {
+    // const matrix = [
+    //     [1, 3, 4, 5],
+    //     [2, 5, 6, 7],
+    //     [1, 9, 6, 5],
+    //     [8, 8, 5, 7],
+    // ];
     const matrix = [
-        [1, 3, 4, 5],
-        [2, 5, 6, 7],
-        [1, 9, 6, 5],
-        [8, 8, 5, 7],
+        [1, 3, 4],
+        [2, 5, 6],
+        [1, 9, 6],
     ];
-    const minPathVal = minPathValsMatrixRecursive(
+    // const minPathVal = minPathValsMatrixRecursive(
+    //     matrix.length,
+    //     matrix[0].length,
+    //     matrix
+    // );
+    const minPathVal = minPathValsMatrixTab(
         matrix.length,
         matrix[0].length,
         matrix
