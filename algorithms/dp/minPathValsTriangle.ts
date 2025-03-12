9f7142d9c7f83a6dc507084cdc4e49c641480af0
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-03-12 15:52:20 -0400

    Add minPathValsTriangleRecursive new approach
    Start recursion from 0,0 of row and height.

--- algorithms/dp/minPathValsTriangle.ts
+++ algorithms/dp/minPathValsTriangle.ts
@@ -4,66 +4,41 @@
  * This means each downward movement only has 2 possibilities.
  *
  * @param triangle integer 2D array. Each row on one more element than previous.
- * @param h integer The height of the triangle
- * @param entryIndex integer Signals which child to continue the path in current row
+ * @param height integer The height of the triangle
+ * @param row integer Signals which child to continue the path in current row
  * @returns minPathVal integer the value of the min path.
  */
-const minPathValsTriangleRecursive = function (
-    h: number,
-    triangle: number[][],
-    entryIndex: number
+function minPathValsTriangleRecursive(
+    height: number,
+    row: number,
+    triangle: number[][]
 ) {
-    const currentRow = triangle[h - 1];
-    if (h === 1) {
-        return currentRow[0];
+    const triangleHeight = triangle.length;
+    const currentValue = triangle[height][row];
+
+    if (height === triangleHeight - 1) {
+        return currentValue;
     }
 
-    let currentMin: number,
-        prevRowIndexMin: number,
-        prevRowIndexMinusOneMin: number = 0;
-    if (entryIndex === currentRow.length - 1) {
-        currentMin =
-            currentRow[entryIndex] +
-            minPathValsTriangleRecursive(h - 1, triangle, entryIndex - 1);
-    } else if (entryIndex - 1 < 0) {
-        currentMin =
-            currentRow[entryIndex] +
-            minPathValsTriangleRecursive(h - 1, triangle, entryIndex);
-    } else {
-        prevRowIndexMinusOneMin =
-            currentRow[entryIndex] +
-            minPathValsTriangleRecursive(h - 1, triangle, entryIndex - 1);
-        prevRowIndexMin =
-            currentRow[entryIndex] +
-            minPathValsTriangleRecursive(h - 1, triangle, entryIndex);
-        currentMin = Math.min(prevRowIndexMin, prevRowIndexMinusOneMin);
-    }
+    const downTotal: number =
+        currentValue + minPathValsTriangleRecursive(height + 1, row, triangle);
 
-    return currentMin;
-};
+    const diagonalDownTotal: number =
+        currentValue +
+        minPathValsTriangleRecursive(height + 1, row + 1, triangle);
+
+    return Math.min(downTotal, diagonalDownTotal);
+}
 
 export function minPathValsTriangleTab(h: number, triangle: number[][]) {}
-
-function generateInput() {}
 
 export default function main() {
     const triangle = [[1], [2, 5], [1, 9, 6]];
     // [ [ 1 ],
     // , [ 2, 5 ],
     // , [ 1, 9, 6 ] ]
-    const lastRow = triangle[triangle.length - 1];
-    let overallMin = -1;
-    for (let index in lastRow) {
-        const currentMin = minPathValsTriangleRecursive(
-            triangle.length,
-            triangle,
-            parseInt(index)
-        );
-        if (overallMin === -1) {
-            overallMin = currentMin;
-        } else {
-            overallMin = Math.min(overallMin, currentMin);
-        }
-    }
+
+    const overallMin = minPathValsTriangleRecursive(0, 0, triangle);
+    console.log(overallMin);
     return overallMin;
 }
