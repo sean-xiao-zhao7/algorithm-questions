d76a72e758c9e040ae25523ad89d94ae72937bba
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-04-03 14:51:45 -0400

    Add maxNinjaChocoRecursive first attempt

---
+++ algorithms/dp/maxNinjaChoco.ts
@@ -0,0 +1,75 @@
+/**
+ * Find the max path for 2 ninjas in a matrix from top to bottom row.
+ *
+ * @param matrix integer 2D array.
+ * @param row integer the row both ninjas are on this iteration.
+ * @param ninja1Col integer The col the first ninja is on this iteration.
+ * @param ninja2Col integer The col the second ninja is on this iteration.
+ * @returns maxPathVal integer the value of the max path.
+ */
+function maxNinjaChocoRecursive(
+    row: number,
+    ninja1Col: number,
+    ninja2Col: number,
+    matrix: number[][],
+    cache: number[][][]
+) {
+    if (row === matrix.length - 1) {
+        if (ninja1Col === ninja2Col) return matrix[row][ninja1Col];
+        else return matrix[row][ninja1Col] + matrix[row][ninja2Col];
+    }
+
+    let overAllMax = -1;
+    for (
+        let nextColNinja1Diff = -1;
+        nextColNinja1Diff < 2;
+        nextColNinja1Diff++
+    ) {
+        for (
+            let nextColNinja2Diff = -1;
+            nextColNinja2Diff < 2;
+            nextColNinja2Diff++
+        ) {
+            let currentMax = -1;
+            const nextColNinja1 = ninja1Col + nextColNinja1Diff;
+            const nextColNinja2 = ninja2Col + nextColNinja2Diff;
+            if (nextColNinja1 === nextColNinja2) {
+                currentMax = matrix[row][nextColNinja1];
+            } else {
+                currentMax =
+                    matrix[row][nextColNinja1] + matrix[row][nextColNinja2];
+            }
+            currentMax += maxNinjaChocoRecursive(
+                row + 1,
+                nextColNinja1,
+                nextColNinja2,
+                matrix,
+                cache
+            );
+            overAllMax = Math.max(overAllMax, currentMax);
+        }
+    }
+    cache[row][ninja1Col][ninja2Col] = overAllMax;
+    return overAllMax;
+}
+
+export function maxNinjaChocoTab(matrix: number[][]) {}
+
+export default function main() {
+    const matrix = [
+        [1, 3, 12, 3],
+        [2, 53, 1, 4],
+        [10, 9, 6, 10],
+        [0, 8, 1, 13],
+    ];
+
+    const cache: number[][][] = [];
+    matrix.forEach(() => {
+        cache.push([[]]);
+    });
+
+    const result = maxNinjaChocoRecursive(0, 0, 0, matrix, cache);
+    console.log(result);
+}
+
+main();
