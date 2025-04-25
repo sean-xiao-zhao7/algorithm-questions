dc176e2034f4aa5b6eb272064fd6d9a29cdf4c08
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-04-25 17:29:32 -0400

    Add starter for findK

---
+++ algorithms/dp/sequences/findK.ts
@@ -0,0 +1,19 @@
+/**
+ * Determine if any sub-sequence of input array sums up to K.
+ *
+ * @param array integer 1D array.
+ * @param k the target.
+ * @returns able integer if k can be summed up to.
+ */
+function findK(array: number[], k: number) {
+    let able = false;
+    return able;
+}
+
+export default function main() {
+    const array = [1, 3, 12, 3, 2, 53, 1, 4];
+    const result = findK(array, 5);
+    console.log(result);
+}
+
+main();
