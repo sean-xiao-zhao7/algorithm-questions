a0eb64d7b7385339071945d1ef07301cc2cd4701
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-04-30 17:49:02 -0400

    Add findK recursive

--- algorithms/dp/sequences/findK.ts
+++ algorithms/dp/sequences/findK.ts
@@ -6,13 +6,33 @@
  * @returns able integer if k can be summed up to.
  */
 function findK(array: number[], k: number) {
-    let able = false;
-    return able;
+    if (k === 0) {
+        return true;
+    }
+
+    if (k < 0) {
+        return false;
+    }
+
+    if (array.length === 0) {
+        if (array[0] === k) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // consider current element
+    const remainingSum = k - array[0];
+    const remainingArray = array.slice(1);
+    const considerResult = findK(remainingArray, remainingSum);
+    const dontConsiderResult = findK(remainingArray, k);
+    return considerResult || dontConsiderResult;
 }
 
 export default function main() {
     const array = [1, 3, 12, 3, 2, 53, 1, 4];
-    const result = findK(array, 5);
+    const result = findK(array, 50);
     console.log(result);
 }
 
