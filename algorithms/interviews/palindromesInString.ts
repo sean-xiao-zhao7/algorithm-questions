ad6b3b8aafb9fabf6c67007450e5eadebc9920c2
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-05-01 12:41:34 -0400

    Add initial cache for countPalindromesInStringRecursive

--- algorithms/interviews/palindromesInString.ts
+++ algorithms/interviews/palindromesInString.ts
@@ -4,20 +4,49 @@
  * Example: ["a","a","b","b","a"] (aabba) as input would return 8.
  * The possible palindromes are a, a, b, b, a, aa, bb, abba (you don’t need to return this part).
  * */
-function countPalindromesInStringRecursive(input: string[]) {
-    if (input.length === 1) {
-        return 1;
-    }
-
+function countPalindromesInStringRecursive(
+    input: string[],
+    cache: number[][],
+    startIdx: number,
+    endIdx: number
+) {
     if (input.length === 2) {
-        return 2;
+        return isPalindrome(input);
+    }
+    if (cache[startIdx][endIdx] !== -1) {
+        return 0;
     }
 
-    if (isPalindrome(input)) {
-        return 1 + countPalindromesInStringRecursive(input.slice(1));
-    } else {
-        return 0 + countPalindromesInStringRecursive(input.slice(1));
+    const isCurrentPalidrome = isPalindrome(input);
+    cache[startIdx][endIdx] = isCurrentPalidrome;
+
+    let removeFirstCount = 0,
+        removeLastCount = 0;
+    removeFirstCount = countPalindromesInStringRecursive(
+        input.slice(1),
+        cache,
+        startIdx + 1,
+        endIdx
+    );
+    removeLastCount = countPalindromesInStringRecursive(
+        input.slice(0, -1),
+        cache,
+        startIdx,
+        endIdx - 1
+    );
+    return isCurrentPalidrome + removeFirstCount + removeLastCount;
+}
+
+function isPalindrome(input: string[]) {
+    let start = 0,
+        end = input.length - 1;
+    while (start <= end) {
+        if (input[start] !== input[end]) return 0;
+        start++;
+        end--;
     }
+    console.log(input);
+    return 1;
 }
 
 function countPalindromesInStringTabulation(
@@ -25,21 +54,23 @@ function countPalindromesInStringTabulation(
     cache: { string: boolean } | {}
 ) {}
 
-function isPalindrome(input: string[]) {
-    let start = 0,
-        end = input.length - 1;
-    while (start < end) {
-        if (input[start] !== input[end]) return false;
-        start++;
-        end--;
-    }
-    return true;
-}
-
 export default function main() {
     const stringInput = ["a", "a", "b", "b", "a"];
-    const result = countPalindromesInStringRecursive(stringInput);
+    const cache: number[][] = [];
+    stringInput.forEach((_, idx) => {
+        const endIdxArray: number[] = [];
+        stringInput.forEach(() => endIdxArray.push(-1));
+        cache[idx] = endIdxArray;
+    });
+    const result =
+        countPalindromesInStringRecursive(
+            stringInput,
+            cache,
+            0,
+            stringInput.length - 1
+        ) + stringInput.length;
     console.log("Total palindromes is " + result);
+    console.log(cache);
 }
 
 main();
