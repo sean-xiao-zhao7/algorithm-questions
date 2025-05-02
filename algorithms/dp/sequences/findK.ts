4567b6b001067fd04ab172ac477659cdb86d943e
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-05-02 12:19:10 -0400

    Add n * target cache for findK

--- algorithms/dp/sequences/findK.ts
+++ algorithms/dp/sequences/findK.ts
@@ -5,7 +5,7 @@
  * @param k the target.
  * @returns able integer if k can be summed up to.
  */
-function findKRecursive(array: number[], k: number) {
+function findKRecursive(array: number[], k: number, cache: number[][]) {
     if (k === 0) {
         return true;
     }
@@ -26,15 +26,25 @@ function findKRecursive(array: number[], k: number) {
     const remainingSum = k - array[0];
     const remainingArray = array.slice(1);
 
-    const considerResult = findKRecursive(remainingArray, remainingSum);
-    const dontConsiderResult = findKRecursive(remainingArray, k);
+    const considerResult = findKRecursive(remainingArray, remainingSum, cache);
+    const dontConsiderResult = findKRecursive(remainingArray, k, cache);
 
     return considerResult || dontConsiderResult;
 }
 
 export default function main() {
     const array = [1, 3, 12, 3, 2, 53, 1, 4];
-    const result = findKRecursive(array, 50);
+    const target = 5;
+    const cache: number[][] = [];
+    array.forEach((_, idx) => {
+        const row: number[] = [];
+        for (let idx = 0; idx < target; idx++) {
+            row[idx] = -1;
+        }
+        cache.push(row);
+    });
+    console.log(cache);
+    const result = findKRecursive(array, target, cache);
     console.log(result);
 }
 
