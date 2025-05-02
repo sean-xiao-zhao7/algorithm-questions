d847155ecfa9e248654e50bcd62e7d60f520f5f7
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-05-02 12:58:44 -0400

    Fix cache n and target max index out of bound error

--- algorithms/dp/sequences/findK.ts
+++ algorithms/dp/sequences/findK.ts
@@ -6,20 +6,25 @@
  * @returns able integer if k can be summed up to.
  */
 function findKRecursive(array: number[], k: number, cache: number[][]) {
-    if (k === 0) {
-        return true;
-    }
-
     if (k < 0) {
         return false;
     }
 
-    if (array.length === 0) {
+    if (k === 0) {
+        return true;
+    }
+
+    if (array.length === 1) {
         if (array[0] === k) {
             return true;
         } else {
             return false;
         }
+    }
+
+    const length = array.length - 1;
+    if (cache[length][k] !== -1) {
+        return cache[length][k] === 1 ? true : false;
     }
 
     // consider current element
@@ -29,23 +34,24 @@ function findKRecursive(array: number[], k: number, cache: number[][]) {
     const considerResult = findKRecursive(remainingArray, remainingSum, cache);
     const dontConsiderResult = findKRecursive(remainingArray, k, cache);
 
-    return considerResult || dontConsiderResult;
+    const overallResult = considerResult || dontConsiderResult;
+    cache[length][k] = overallResult ? 1 : 0;
+    return overallResult;
 }
 
 export default function main() {
     const array = [1, 3, 12, 3, 2, 53, 1, 4];
     const target = 5;
     const cache: number[][] = [];
-    array.forEach((_, idx) => {
+    array.forEach((_) => {
         const row: number[] = [];
-        for (let idx = 0; idx < target; idx++) {
+        for (let idx = 0; idx < target + 1; idx++) {
             row[idx] = -1;
         }
         cache.push(row);
     });
-    console.log(cache);
     const result = findKRecursive(array, target, cache);
-    console.log(result);
+    console.log(result, cache);
 }
 
 main();
