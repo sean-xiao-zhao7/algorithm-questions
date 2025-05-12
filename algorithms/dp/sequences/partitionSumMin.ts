76297d0f298c41103694aa326b9b81de7fd6a9ac
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-05-12 16:34:06 -0400

    Add non-zero base case in partitionSumMinRecursive

--- algorithms/dp/sequences/partitionSumMin.ts
+++ algorithms/dp/sequences/partitionSumMin.ts
@@ -6,11 +6,11 @@
  */
 function partitionSumMinRecursive(array: number[], targetSum: number) {
     if (targetSum === 0) {
-        return true;
+        return 0;
     }
 
     if (array.length === 1) {
-        return array[0] === targetSum;
+        return Math.abs(array[0] - targetSum);
     }
 
     const currentElement = array[0];
@@ -20,7 +20,7 @@ function partitionSumMinRecursive(array: number[], targetSum: number) {
     const takeCurrentBool = partitionSumMinRecursive(newArray, newTarget);
     const dontTakeCurrentBool = partitionSumMinRecursive(newArray, targetSum);
 
-    return takeCurrentBool || dontTakeCurrentBool;
+    return Math.min(takeCurrentBool, dontTakeCurrentBool);
 }
 
 /**
@@ -29,12 +29,8 @@ function partitionSumMinRecursive(array: number[], targetSum: number) {
 export default function main() {
     const array = [2, 3, 3, 3, 4, 5];
     const entireSum = array.reduce((x, c) => x + c, 0);
-    if (entireSum % 2 !== 0) {
-        console.log(entireSum, false);
-    } else {
-        const recursiveResult = partitionSumMinRecursive(array, entireSum / 2);
-        console.log(recursiveResult);
-    }
+    const recursiveResult = partitionSumMinRecursive(array, entireSum / 2);
+    console.log(recursiveResult);
 }
 
 main();
