c35f6f32745f54cee8ca9b4072506e9a21ce7c47
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-05-12 14:44:25 -0400

    Add partitionSumRecursive starter

---
+++ algorithms/dp/sequences/partitionSum.ts
@@ -0,0 +1,22 @@
+/**
+ * Determine if input array can be partitioned by 2, where each sub-array sums to the same sum.
+ *
+ * @param array integer 1D array.
+ */
+function partitionSumRecursive(array: number[]) {}
+
+/**
+ * Bottom up. From array of length 1 and target of 0, to length of N and target of "target".
+ * @param array
+ */
+function partitionSumRTabulation() {}
+
+/**
+ * Main exec, with some input preparation.
+ */
+export default function main() {
+    const array = [1, 3, 12, 3, 2, 53, 1, 4];
+    const recursiveResult = partitionSumRecursive(array);
+}
+
+main();
