701ebcbd4a2201b135407918f7605f79e22872b1
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-05-12 14:59:48 -0400

    Complete partitionSumRecursive

--- algorithms/dp/sequences/partitionSum.ts
+++ algorithms/dp/sequences/partitionSum.ts
@@ -3,7 +3,48 @@
  *
  * @param array integer 1D array.
  */
-function partitionSumRecursive(array: number[]) {}
+function partitionSumRecursive(
+    array: number[],
+    left: number[],
+    right: number[],
+    leftSum: number,
+    rightSum: number
+) {
+    const currentElement = array[0];
+
+    if (array.length === 1) {
+        return (
+            leftSum + currentElement === rightSum ||
+            leftSum === rightSum + currentElement
+        );
+    }
+
+    const remainingArray = array.slice(1);
+
+    // add current element to left
+    const newLeftSum = leftSum + currentElement;
+    const newLeft = [...left, currentElement];
+    const takeCurrentLeft = partitionSumRecursive(
+        remainingArray,
+        newLeft,
+        right,
+        newLeftSum,
+        rightSum
+    );
+
+    // add current element to right
+    const newRightSum = rightSum + currentElement;
+    const newRight = [...right, currentElement];
+    const takeCurrentRight = partitionSumRecursive(
+        remainingArray,
+        left,
+        newRight,
+        leftSum,
+        newRightSum
+    );
+
+    return takeCurrentLeft || takeCurrentRight;
+}
 
 /**
  * Bottom up. From array of length 1 and target of 0, to length of N and target of "target".
@@ -15,8 +56,10 @@ function partitionSumRTabulation() {}
  * Main exec, with some input preparation.
  */
 export default function main() {
-    const array = [1, 3, 12, 3, 2, 53, 1, 4];
-    const recursiveResult = partitionSumRecursive(array);
+    const array = [2, 3, 3, 3, 4, 5];
+    // [1] [3, 12, 3, 2, 53, 1, 4];
+    const recursiveResult = partitionSumRecursive(array, [], [], 0, 0);
+    console.log(recursiveResult);
 }
 
 main();
