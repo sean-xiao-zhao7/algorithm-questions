5f6e307e98e493faabfc6b1ce8213cf82ea731b7
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2025-05-12 15:54:52 -0400

    Use cache for partitionSumRecursive

--- algorithms/dp/sequences/partitionSum.ts
+++ algorithms/dp/sequences/partitionSum.ts
@@ -6,9 +6,14 @@
 function partitionSumRecursive(
     array: number[],
     leftSum: number,
-    rightSum: number
+    rightSum: number,
+    cache: number[][][]
 ) {
     const currentElement = array[0];
+
+    if (cache[array.length - 1][leftSum][rightSum] !== -1) {
+        return cache[array.length - 1][leftSum][rightSum];
+    }
 
     if (array.length === 1) {
         return (
@@ -24,7 +29,8 @@ function partitionSumRecursive(
     const takeCurrentLeft = partitionSumRecursive(
         remainingArray,
         newLeftSum,
-        rightSum
+        rightSum,
+        cache
     );
 
     // add current element to right
@@ -32,10 +38,13 @@ function partitionSumRecursive(
     const takeCurrentRight = partitionSumRecursive(
         remainingArray,
         leftSum,
-        newRightSum
+        newRightSum,
+        cache
     );
 
-    return takeCurrentLeft || takeCurrentRight;
+    cache[array.length - 1][leftSum][rightSum] =
+        takeCurrentLeft || takeCurrentRight ? 1 : 0;
+    return cache[array.length - 1][leftSum][rightSum];
 }
 
 /**
@@ -43,7 +52,30 @@ function partitionSumRecursive(
  *
  * @param array integer 1D array.
  */
-function partitionSumRecursiveHalfMethod(array: number[], targetSum: number) {}
+function partitionSumRecursiveHalfMethod(array: number[], targetSum: number) {
+    if (targetSum === 0) {
+        return true;
+    }
+
+    if (array.length === 1) {
+        return array[0] === targetSum;
+    }
+
+    const currentElement = array[0];
+    const newTarget = targetSum - currentElement;
+    const newArray = array.slice(1);
+
+    const takeCurrentBool = partitionSumRecursiveHalfMethod(
+        newArray,
+        newTarget
+    );
+    const dontTakeCurrentBool = partitionSumRecursiveHalfMethod(
+        newArray,
+        targetSum
+    );
+
+    return takeCurrentBool || dontTakeCurrentBool;
+}
 
 /**
  * Bottom up. From array of length 1 and target of 0, to length of N and target of "target".
@@ -55,16 +87,28 @@ function partitionSumRTabulation() {}
  * Main exec, with some input preparation.
  */
 export default function main() {
-    const array = [2, 3, 3, 3, 4, 4];
+    const array = [2, 3, 3, 3, 4, 5];
     const entireSum = array.reduce((x, c) => x + c, 0);
     if (entireSum % 2 !== 0) {
         console.log(entireSum, false);
     } else {
-        // const recursiveResult = partitionSumRecursive(array, 0, 0);
-        const recursiveResult = partitionSumRecursiveHalfMethod(
-            array,
-            entireSum / 2
-        );
+        const cache: number[][][] = [];
+        array.forEach(() => {
+            let lefts: number[][] = [];
+            for (let left = 0; left < entireSum; left++) {
+                lefts[left] = [];
+                for (let right = 0; right < entireSum; right++) {
+                    lefts[left][right] = -1;
+                }
+            }
+            cache.push(lefts);
+        });
+        const recursiveResult = partitionSumRecursive(array, 0, 0, cache);
+
+        // const recursiveResult = partitionSumRecursiveHalfMethod(
+        //     array,
+        //     entireSum / 2
+        // );
         console.log(recursiveResult);
     }
 }
